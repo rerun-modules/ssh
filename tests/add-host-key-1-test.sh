f1dbe3f08e1bd4f828d11474719a934666c4b353
Anthony Shortland <anthony@dtosolutions.com>
2012-12-03 16:04:57 -0800

    Closes #6 by adding support for ssh-keyscan(1) to the add-host-key command.

--- tests/add-host-key-1-test.sh
+++ tests/add-host-key-1-test.sh
@@ -14,7 +14,8 @@
 
 describe "add-host-key"
 
-it_works_with_a_new_key_and_a_duplicate() {
+# Tests generating a DSA public host key and adding it to a temporary known hosts file.
+it_works_with_a_new_and_duplicate_key() {
   # Generate a new key-pair into a temporary directory.
   TMPDIR=$(mktemp -d)
   rerun ssh:keygen --key-file ${TMPDIR}/id_dsa
@@ -22,23 +23,48 @@ it_works_with_a_new_key_and_a_duplicate() {
   # Get the fields from the generated public key file.
   TYPE=$(cut -d' ' -f1 < ${TMPDIR}/id_dsa.pub | cut -d- -f2)
   PUBLIC_KEY=$(cut -d' ' -f2 < ${TMPDIR}/id_dsa.pub)
-  COMMENT=$(cut -d' ' -f3- < ${TMPDIR}/id_dsa.pub)
 
   # Add the key to a new empty known hosts file and make sure its the same as the original public key:
-  rerun ssh:add-host-key --public-key "${PUBLIC_KEY}" --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname localhost
+  rerun ssh:add-host-key --host-key "${PUBLIC_KEY}" --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname localhost
   echo "localhost ssh-${TYPE} ${PUBLIC_KEY}" > ${TMPDIR}/known_hosts.test
   diff ${TMPDIR}/known_hosts.test ${TMPDIR}/.ssh/known_hosts
 
   # Add the key a second time and make sure its the same as the original public key thus proving a duplicate entry was avoided:
-  rerun ssh:add-host-key --public-key "${PUBLIC_KEY}" --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname localhost
+  rerun ssh:add-host-key --host-key "${PUBLIC_KEY}" --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname localhost
   echo "localhost ssh-${TYPE} ${PUBLIC_KEY}" > ${TMPDIR}/known_hosts.test
   diff ${TMPDIR}/known_hosts.test ${TMPDIR}/.ssh/known_hosts
 
   # Add the key a third time with a different hostname proving the existing entry is replaced:
-  rerun ssh:add-host-key --public-key "${PUBLIC_KEY}" --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname $HOSTNAME
+  rerun ssh:add-host-key --host-key "${PUBLIC_KEY}" --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname $HOSTNAME
   echo "$HOSTNAME ssh-${TYPE} ${PUBLIC_KEY}" > ${TMPDIR}/known_hosts.test
   diff ${TMPDIR}/known_hosts.test ${TMPDIR}/.ssh/known_hosts
 
   rm -rf ${TMPDIR}
 }
 
+# Tests using ssh-keyscan(1) to get the localhost's DSA public host key and add it to a temporary known hosts file
+it_works_when_no_host_key_is_provided() {
+  # Generate a new key-pair into a temporary directory.
+  TMPDIR=$(mktemp -d)
+
+  # Get the fields from the generated public key file.
+  TYPE=dsa
+  PUBLIC_KEY=$(ssh-keyscan -t dsa localhost 2>/dev/null | cut  -d' ' -f3)
+
+  # Add the key to a new empty known hosts file and make sure its the same as the original public key:
+  rerun ssh:add-host-key --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname localhost
+  echo "localhost ssh-${TYPE} ${PUBLIC_KEY}" > ${TMPDIR}/known_hosts.test
+  diff ${TMPDIR}/known_hosts.test ${TMPDIR}/.ssh/known_hosts
+
+  # Add the key a second time and make sure its the same as the original public key thus proving a duplicate entry was avoided:
+  rerun ssh:add-host-key --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname localhost
+  echo "localhost ssh-${TYPE} ${PUBLIC_KEY}" > ${TMPDIR}/known_hosts.test
+  diff ${TMPDIR}/known_hosts.test ${TMPDIR}/.ssh/known_hosts
+
+  # Add the key a third time with a different hostname proving the existing entry is replaced:
+  rerun ssh:add-host-key --known-hosts-file ${TMPDIR}/.ssh/known_hosts --type ${TYPE} --hostname $HOSTNAME
+  echo "$HOSTNAME ssh-${TYPE} ${PUBLIC_KEY}" > ${TMPDIR}/known_hosts.test
+  diff ${TMPDIR}/known_hosts.test ${TMPDIR}/.ssh/known_hosts
+
+  rm -rf ${TMPDIR}
+}
